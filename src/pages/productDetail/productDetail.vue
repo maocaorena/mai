aab12a6142a6efa754d0c1fc9b6fd10a0a22d678
maocaorena <han.mao@bianxianmao.com>
2018-07-02 10:38:20 +0800

    多图轮播

--- src/pages/productDetail/productDetail.vue
+++ src/pages/productDetail/productDetail.vue
@@ -132,7 +132,7 @@
                     Indicator.close();
                     if (res.successed) {
                         this.detail = res.returnValue;
-                        this.bannerList.push(res.returnValue.image)
+                        this.bannerList.push(...res.returnValue.images, 'https://klbf-file.oss-cn-hangzhou.aliyuncs.com/2018/06/25/6a371f2809f442aca288a1241e7213e8.png')
                     }
                 }).then(() => {
                     let mySwiper = new Swiper('#detailSwiper', {
